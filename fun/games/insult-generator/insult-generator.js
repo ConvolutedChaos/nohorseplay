eb5e02f4f1231634e813b06770f16d4a542713b8
ConvolutedChaos <evan.m.griebel@gmail.com>
2023-01-26 19:25:10 -0600

    e

---
+++ fun/games/insult-generator/insult-generator.js
@@ -0,0 +1,9 @@
+var randomBodyParts = ["Face", "Nose", "Hair"];
+var randomAdjectives = ["Smelly", "Boring", "Stupid"];
+var randomWords = ["Fly", "Marmot", "Stick", "Monkey", "Rat"];
+var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
+var randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
+var randomWord = randomWords[Math.floor(Math.random() * 5)];
+var randomInsult = "Your " + randomBodyPart + " is like a " +
+randomAdjective + " " + randomWord + "!!!";
+randomInsult;
