b9019235818ae50ddaa9076695f45bfede344036
ConvolutedChaos <evan.m.griebel@gmail.com>
2023-04-29 20:00:14 -0500

    Camera

--- A-Random-Website-once-again/wow/os-emulators/edogos/files/camera.js
+++ A-Random-Website-once-again/wow/os-emulators/edogos/files/camera.js
@@ -1,58 +1,15 @@
 var width = 320;
 var height = 0;
 var streaming = false;
-var localstream = null;
 
-startCameraButton.onclick = start;
-takePictureButton.onclick = takepicture;
-
-navigator.mediaDevices.enumerateDevices()
-        .then(gotDevices)
+navigator.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(function (stream) {
+            video.srcObject = stream;
+            video.play();
+        })
         .catch(function (err) {
-            console.log("An error occured while getting device list! " + err);
+            console.log("An error occured! " + err);
         });
-
-function gotDevices(deviceInfos) {
-    while (videoSelect.firstChild) {
-        videoSelect.removeChild(videoSelect.firstChild);
-    }
-
-    for (var i = 0; i !== deviceInfos.length; ++i) {
-        var deviceInfo = deviceInfos[i];
-        var option = document.createElement('option');
-        option.value = deviceInfo.deviceId;
-        if (deviceInfo.kind === 'videoinput') {
-            option.text = deviceInfo.label || 'Camera ' + (videoSelect.length + 1);
-            videoSelect.appendChild(option);
-        }
-    }
-}
-
-function start() {
-    stopVideo();
-    clearphoto();
-    var videoSource = videoSelect.value;
-    var constraints = {
-        audio: false,
-        video: {deviceId: videoSource ? {exact: videoSource} : undefined}
-    };
-    navigator.mediaDevices.getUserMedia(constraints).
-            then(gotStream).then(gotDevices).catch(handleError);
-}
-
-
-
-function gotStream(stream) {
-    localstream = stream;
-    video.srcObject = stream;
-    video.play();
-    // Refresh button list in case labels have become available
-    return navigator.mediaDevices.enumerateDevices();
-}
-
-function handleError(error) {
-    console.log('navigator.getUserMedia error: ', error);
-}
 
 video.addEventListener('canplay', function (ev) {
     if (!streaming) {
@@ -64,6 +21,11 @@ video.addEventListener('canplay', function (ev) {
 
         streaming = true;
     }
+}, false);
+
+startbutton.addEventListener('click', function (ev) {
+    takepicture();
+    ev.preventDefault();
 }, false);
 
 clearphoto();
@@ -84,30 +46,20 @@ function takepicture() {
         var dataURL = canvas.toDataURL("image/jpeg", 0.95);
         if (dataURL && dataURL != "data:,") {
             var fileName = generateImageName();
-            fileName = fileName + ".txt"
             uploadimage(dataURL, fileName);
         } else {
-            console.log("Image not available");
+            alert("Image not available");
         }
     } else {
         clearphoto();
     }
 }
 
-    function generateImageName() {
+function generateImageName() {
     // ... generate image name logic here ...
     return imageName;
 }
 
 function uploadimage(dataurl, filename) {
     // ... upload logic here ...
-}
-
-function stopVideo() {
-    if (localstream) {
-        localstream.getTracks().forEach(function (track) {
-            track.stop();
-            localstream = null;
-        });
-    }
 }
