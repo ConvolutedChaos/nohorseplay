8d6f803a83f35c3445025b8a7fe65305f2ed485f
ConvolutedChaos <evan.m.griebel@gmail.com>
2025-02-03 21:06:48 -0600

    Physics Fun 0.5, a tab disguise, and an anti-close

--- games/PhysicsFun/ServiceWorker.js
+++ games/PhysicsFun/ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Nohorseplay-Physics Fun-0.1";
+const cacheName = "Nohorseplay-Physics Fun-0.5";
 const contentToCache = [
     "Build/PhysicsFun.loader.js",
     "Build/PhysicsFun.framework.js",
