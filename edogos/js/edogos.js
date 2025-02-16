23ac876e154b0e3e9c22837d6a0db5c64f5b621e
ConvolutedChaos <evan.m.griebel@gmail.com>
2025-02-15 19:27:28 -0600

    E-Dog OS v0.3.5

--- edogos/js/edogos.js
+++ edogos/js/edogos.js
@@ -2,7 +2,7 @@ var startMenu = document.getElementById("startMenu");
 
 var wallpaperSuccessMessage = "Wallpaper successfully changed.";
 
-var tipMessage = "Tips:\n\nIf you enter in a web page address and it doesn't work, look at these tips:\n\n1) _'s server IP adress could not be found.\nThis means that the page doesn't exist. Try checking your spelling.\n\n2) __ refused to connect.\nThis means that the page exists, however it cannot be viewed in an Iframe, in other words the page cannot be viewed in Bacon Explorer.\nExamples: YouTube, Google, DuckDuckGo, Amazon, Facebook."
+var tipMessage = "Tips:\n\nIf you enter in a web page address and it doesn't work, look at these tips:\n\n1) _'s server IP address could not be found.\nThis means that the page doesn't exist. Try checking your spelling.\n\n2) __ refused to connect.\nThis means that the page exists, however it cannot be viewed in an Iframe, in other words the page cannot be viewed in Bacon Explorer.\nExamples: YouTube, Google, DuckDuckGo, Amazon, Facebook."
 
 function openStartMenu() {
     if (startMenu.style.display === "block") {
@@ -305,6 +305,110 @@ function replayVideo() {
     video.play();
 }
 
-document.getElementById("userAgent").innerHTML = navigator.userAgent;
+function resetVideo() {
+    video.pause();
+    video.currentTime = 0;
+}
 
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+function calculateResult() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch {
+        crashSystem('INVALID_CALC_EXPRESSION');
+    }
+}
+
+const folders = {
+    "Home": [{ name: "Desktop", icon: "media/img/user-desktop.png", type: "folder" }, { name: "Documents", icon: "media/img/folder-documents.png", type: "folder" }, { name: "Downloads", icon: "media/img/folder-download.png", type: "folder" }, { name: "Music", icon: "media/img/folder-music.png", type: "folder" }, { name: "Pictures", icon: "media/img/folder-pictures.png", type: "folder" }, { name: "Videos", icon: "media/img/folder-videos.png", type: "folder" }],
+    "Documents": [{ name: "Reports", icon: "media/img/filetypes/text-x-generic.png", type: "file" }, { name: "Notes", icon: "media/img/filetypes/text-x-generic.png", type: "file" }, { name: "Projects", icon: "media/img/filetypes/text-x-generic.png", type: "file" }],
+    "Downloads": [{ name: "File1.zip", icon: "media/img/filetypes/package-x-generic.png", type: "file" }, { name: "Setup.exe", icon: "media/img/filetypes/application-x-executable.png", type: "file" }],
+    "Pictures": [{ name: "Image1.png", icon: "media/img/filetypes/image-x-generic.png", type: "file" }, { name: "Photo.jpg", icon: "media/img/filetypes/image-x-generic.png", type: "file" }],
+    "Music": [{ name: "Song1.mp3", icon: "media/img/filetypes/audio-x-generic.png", type: "file" }, { name: "Song2.mp3", icon: "media/img/filetypes/audio-x-generic.png", type: "file" }],
+    "Videos": [{ name: "raisins.mp4", icon: "media/img/raisins-mp4-thumb.png", type: "file" }, { name: "Clip.avi", icon: "media/img/filetypes/video-x-generic.png", type: "file" }]
+};
+
+let history = [];
+let historyIndex = -1;
+
+function openFolder(folderName) {
+    var fileExplorerTitleText = document.getElementById("fileExplorerTitleBarText");
+    fileExplorerTitleText.innerText = folderName;
+    if (historyIndex === -1 || history[historyIndex] !== folderName) {
+        history = history.slice(0, historyIndex + 1);
+        history.push(folderName);
+        historyIndex++;
+    }
+
+    const panel = document.getElementById("mainPanel");
+    panel.innerHTML = "";
+
+    document.querySelectorAll(".sidebar li").forEach(li => {
+        li.classList.toggle("active", li.textContent === folderName);
+    });
+
+    if (folders[folderName]) {
+        folders[folderName].forEach(item => {
+            const div = document.createElement("div");
+            div.classList.add("item");
+            div.innerHTML = `<img src='${item.icon}' alt='folder'><br>${item.name}`;
+            div.ondblclick = () => {
+                if (item.type === "folder") {
+                    openFolder(item.name);
+                } else {
+                    openFile(item.name);
+                }
+            };
+            panel.appendChild(div);
+        });
+        document.getElementById("fileCount").textContent = folders[folderName].length + " items";
+    }
+}
+
+function openFile(fileName) {
+    console.log("Attempting to open " + fileName + "...");
+    if (fileName === "raisins.mp4") {
+        openApp('mediaPlayer', 'mediaPlayerIcon');
+    }
+}
+
+function goBack() {
+    if (historyIndex > 0) {
+        historyIndex--;
+        openFolder(history[historyIndex]);
+    }
+}
+
+function goForward() {
+    if (historyIndex < history.length - 1) {
+        historyIndex++;
+        openFolder(history[historyIndex]);
+    }
+}
+
+function updateZoom(value) {
+    document.querySelectorAll(".item img").forEach(img => {
+        img.style.width = value + "px";
+        img.style.height = value + "px";
+    });
+}
+
+openFolder("Home"); // Load Home by default
+
+
+function crashSystem(errorMessage) {
+    var crashScreenElement = document.getElementById("crashScreen");
+    crashScreenElement.innerHTML = "A problem has been detected and E-Dog OS has been shut down to prevent damage to your computer.<br><br> " + errorMessage + "<br><br> If this is the first time you've seen this error screen, restart your computer. If this screen appears again, follow these steps:<br> Check to make sure any new hardware or software is properly installed. If this is a new installation, ask your hardware or<br> software manufacturer for any updates you might need. If problems continue, disable or remove any newly installed<br> hardware or software. Disable BIOS memory options such as caching or shadowing. If you need to eat bacon, restart<br> your computer, press F8 to select Advanced Startup Options, and then select Eat Bacon.<br><br> Technical Information:<br><br> *** STOP: 0x000000C1 (0x61AF2FF8, 0xEDCCAEBE, 0xB663BAE9, 0x8E2DF694)<br><br>";
+    video.pause();
+    toggleApp('crashScreen', 'crashScreen');
+}
+
+// document.getElementById("userAgent").innerHTML = navigator.userAgent;
+console.log("TIP - any errors or messages that are not coming from E-Dog OS are coming from an embedded page in Bacon Explorer.")
+console.log("wait--why are you in here?")
 console.log("E-Dog OS Successfully loaded");
