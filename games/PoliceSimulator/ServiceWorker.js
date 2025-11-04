267da44e0b61480d05de7dd8c9bb1d2701218813
ConvolutedChaos <evan.m.griebel@gmail.com>
2025-11-03 21:33:15 -0600

    Police Simulator 0.1

---
+++ games/PoliceSimulator/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Nohorseplay-PoliceSimulator-0.1";
+const contentToCache = [
+    "Build/PoliceSimulator.loader.js",
+    "Build/PoliceSimulator.framework.js",
+    "Build/PoliceSimulator.data",
+    "Build/PoliceSimulator.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
