e7e794747f3e22d02776f865e278e7b9e18cb4ac
ConvolutedChaos <evan.m.griebel@gmail.com>
2025-12-31 13:24:01 -0600

    Website Clean 5

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # nohorseplay
 
-## Introduction
-
-Welcome to **nohorseplay**! This personal project of mine features the popular Web Cars and Physics Fun. Also, we have other boredom-curing stuff, too. Just check out our page? :)
-## Features
-
-- Easy to use
-- Boredom-curing things
-- Fun games
+Welcome to **nohorseplay**! This personal project of mine features some fun things, mostly Unity games.
--- fun/games/insult-generator/insult-generator.js
+++
@@ -1,9 +0,0 @@
-var randomBodyParts = ["Face", "Nose", "Hair","Butt","Eye","Chin"];
-var randomAdjectives = ["Smelly", "Boring", "Stupid","Rancid","Jelly","Unfortunate"];
-var randomWords = ["Fly", "Marmot", "Stick", "Monkey", "Rat","Crap","Booger","Thesarus"];
-var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
-var randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
-var randomWord = randomWords[Math.floor(Math.random() * 5)];
-var randomInsult = "Your " + randomBodyPart + " is like a " +
-randomAdjective + " " + randomWord + "!!!";
-alert("Your " + randomBodyPart + " is like a " + randomAdjective + " " + randomWord)
