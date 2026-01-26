d3e7024634293cdedb3496145920e1c01d917cbe
ConvolutedChaos <evan.m.griebel@gmail.com>
2026-01-25 21:52:55 -0600

    Web Cars V3 Preview

---
+++ games/WebCarsV3/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Nohorseplay-WebCarsV3-0.1";
+const contentToCache = [
+    "Build/WebCarsV3.loader.js",
+    "Build/WebCarsV3.framework.js.unityweb",
+    "Build/WebCarsV3.data.unityweb",
+    "Build/WebCarsV3.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
